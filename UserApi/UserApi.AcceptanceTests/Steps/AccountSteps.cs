6107663949b802f0c47c28de9d145ae632bcc321
NPoxon <sendnickmessages@hotmail.com>
2019-03-04 18:07:32 +0100

    Added a skeleton acceptance test project

---
+++ UserApi/UserApi.AcceptanceTests/Steps/AccountSteps.cs
@@ -0,0 +1,20 @@
+﻿using TechTalk.SpecFlow;
+using Testing.Common.Helpers;
+using UserApi.AcceptanceTests.Contexts;
+
+namespace UserApi.AcceptanceTests.Steps
+{
+    [Binding]
+    public sealed class AccountSteps : BaseSteps
+    {
+        private readonly ScenarioContext _context;
+        private readonly AcTestContext _acTestContext;
+        private readonly AccountEndpoints _endpoints = new ApiUriFactory().AccountEndpoints;
+
+        public AccountSteps(ScenarioContext injectedContext, AcTestContext acTestContext)
+        {
+            _context = injectedContext;
+            _acTestContext = acTestContext;
+        }
+    }
+}
---
+++ UserApi/UserApi.AcceptanceTests/Steps/CommonSteps.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+using UserApi.AcceptanceTests.Contexts;
+
+namespace UserApi.AcceptanceTests.Steps
+{
+    [Binding]
+    public sealed class CommonSteps : BaseSteps
+    {
+        private readonly ScenarioContext _context;
+        private readonly AcTestContext _acTestContext;
+
+        public CommonSteps(ScenarioContext injectedContext, AcTestContext acTestContext)
+        {
+            _context = injectedContext;
+            _acTestContext = acTestContext;
+        }
+
+        [When(@"I send the request to the endpoint")]
+        public void WhenISendTheRequestToTheEndpoint()
+        {
+            _acTestContext.Response = _acTestContext.Client().Execute(_acTestContext.Request);
+            if (_acTestContext.Response.Content != null)
+                _acTestContext.Json = _acTestContext.Response.Content;
+        }
+
+        [Then(@"the response should have the status (.*) and success status (.*)")]
+        public void ThenTheResponseShouldHaveTheStatusAndSuccessStatus(HttpStatusCode httpStatusCode, bool isSuccess)
+        {
+            _acTestContext.Response.StatusCode.Should().Be(httpStatusCode);
+            _acTestContext.Response.IsSuccessful.Should().Be(isSuccess);
+        }
+    }
+}
