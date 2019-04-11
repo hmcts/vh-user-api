9c2cf1bd8c6d67d90536f9e4287e35a489987759
muralikaturi <murali.katuri@hmcts.net>
2019-04-11 22:40:15 +0100

    Added MessageEncryptor

---
+++ UserApi/UserApi.UnitTests/CustomJwtToken/MessageEncryptorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using UserApi.Security;
+using UserApi.Security.CustomToken;
+
+namespace UserApi.UnitTests.CustomJwtToken
+{
+    public class MessageEncryptorTest
+    {
+        [Test]
+        public void should_encrypt()
+        {
+            var customJwtTokenConfigSettings = new CustomJwtTokenConfigSettings(1,
+                "W2gEmBn2H7b2FCMIQl6l9rggbJU1qR7luIeAf1uuaY+ik6TP5rN0NEsPVg0TGkroiel0SoCQT7w3cbk7hFrBtA==",
+                string.Empty, string.Empty);
+
+            var messageEncryptor = new MessageEncryptor(customJwtTokenConfigSettings);
+            var id = Guid.NewGuid().ToString("N");
+            var request = $"https://poc.kinly.hmcts.net:{id}:test";
+            var hashRequestTarget = messageEncryptor.HashRequestTarget(request);
+            hashRequestTarget.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void should_fail_authentication()
+        {
+            var customJwtTokenConfigSettings = new CustomJwtTokenConfigSettings(1,
+                "W2gEmBn2H7b2FCMIQl6l9rggbJU1qR7luIeAf1uuaY+ik6TP5rN0NEsPVg0TGkroiel0SoCQT7w3cbk7hFrBtA==",
+                string.Empty, string.Empty);
+
+            var messageEncryptor = new MessageEncryptor(customJwtTokenConfigSettings);
+            var id = Guid.NewGuid().ToString("N");
+            var request = $"https://poc.kinly.hmcts.net:{id}:test";
+            var hashRequestTarget = messageEncryptor.HashRequestTarget(request);
+
+            var request2 = $"https://poc.kinly.hmcts.net:{Guid.NewGuid().ToString("N")}:test";
+            var hashRequestTarget2 = messageEncryptor.HashRequestTarget(request2);
+            hashRequestTarget2.Should().NotBe(hashRequestTarget);
+        }
+    }
+}
---
+++ UserApi/UserApi/Security/MessageEncryptor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using UserApi.Security.CustomToken;
+
+namespace UserApi.Security
+{
+    public class MessageEncryptor
+    {
+        private readonly ICustomJwtTokenConfigSettings _customJwtTokenConfigSettings;
+
+        public MessageEncryptor(ICustomJwtTokenConfigSettings customJwtTokenConfigSettings)
+        {
+            _customJwtTokenConfigSettings = customJwtTokenConfigSettings;
+        }
+
+        public string HashRequestTarget(string requestTarget)
+        {
+            var key = Convert.FromBase64String(_customJwtTokenConfigSettings.Secret);
+            var requestUri = System.Web.HttpUtility.UrlEncode(requestTarget);
+
+            var request = Encoding.UTF8.GetBytes(requestUri);
+            using (var hmac = new HMACSHA256(key))
+            {
+                var computeHash = hmac.ComputeHash(request);
+                return Convert.ToBase64String(computeHash);
+            }
+        }
+    }
+}
