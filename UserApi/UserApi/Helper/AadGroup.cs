f23a8d2ff15067b5e9d9a756ac453f221356d8a1
David Dikman <ddikman@gmail.com>
2019-05-02 16:19:14 +0100

    Removed unused groups from enumeration.

--- UserApi/UserApi/Helper/AadGroup.cs
+++ UserApi/UserApi/Helper/AadGroup.cs
@@ -1,20 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Reflection.Metadata.Ecma335;
-
-namespace UserApi.Helper
+﻿namespace UserApi.Helper
 {
     public enum AadGroup
     {
-        [Display(Name = "VirtualRoomAdministrator")]
-        VirtualRoomAdministrator = 1,
-        [Display(Name = "External")] External = 2,
-        [Display(Name = "Internal")] Internal = 3,
-        [Display(Name = "MoneyClaims")] MoneyClaims = 4,
-        [Display(Name = "FinancialRemedy")] FinancialRemedy = 5,
-        [Display(Name = "SSPR Enabled")] SsprEnabled = 6,
-        [Display(Name = "VirtualRoomProfessionalUser")]
-        VirtualRoomProfessionalUser = 7,
-        [Display(Name = "VirtualRoomJudge")]
-        VirtualRoomJudge = 8
+        VirtualRoomAdministrator,
+        External,
+        MoneyClaims,
+        FinancialRemedy,
+        VirtualRoomProfessionalUser,
+        VirtualRoomJudge
     }
 }
