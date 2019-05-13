cf3c6c894f4e22eaffb1e9403c40e7a43afe9b63
David Dikman <ddikman@gmail.com>
2019-05-13 18:38:09 +0100

    Replaced usages of IOptions

--- UserApi/UserApi.AcceptanceTests/Steps/BaseSteps.cs
+++ UserApi/UserApi.AcceptanceTests/Steps/BaseSteps.cs
@@ -22,32 +22,19 @@ namespace UserApi.AcceptanceTests.Steps
         [BeforeTestRun]
         public static void OneTimeSetup(AcTestContext testContext)
         {
-            var configRootBuilder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables()
-                .AddUserSecrets<Startup>();
+            var azureAdConfiguration = TestConfig.Instance.AzureAd;
+            testContext.TestSettings = TestConfig.Instance.TestSettings;
 
-            var configRoot = configRootBuilder.Build();
-
-            var azureAdConfigurationOptions =
-                Options.Create(configRoot.GetSection("AzureAd").Get<AzureAdConfiguration>());
-            var testSettingsOptions = Options.Create(configRoot.GetSection("Testing").Get<TestSettings>());
-
-            var azureAdConfiguration = azureAdConfigurationOptions.Value;
-            testContext.TestSettings = testSettingsOptions.Value;
-
-            testContext.BearerToken = new TokenProvider(azureAdConfigurationOptions).GetClientAccessToken(
+            testContext.BearerToken = new TokenProvider(azureAdConfiguration).GetClientAccessToken(
                 testContext.TestSettings.TestClientId, testContext.TestSettings.TestClientSecret,
                 azureAdConfiguration.VhUserApiResourceId);
 
-            testContext.GraphApiToken = new TokenProvider(azureAdConfigurationOptions).GetClientAccessToken(
+            testContext.GraphApiToken = new TokenProvider(azureAdConfiguration).GetClientAccessToken(
                 azureAdConfiguration.ClientId, azureAdConfiguration.ClientSecret,
                 "https://graph.microsoft.com");
 
-            var apiTestsOptions =
-                Options.Create(configRoot.GetSection("AcceptanceTestSettings").Get<AcceptanceTestConfiguration>());
-            var apiTestSettings = apiTestsOptions.Value;
-            testContext.BaseUrl = apiTestSettings.UserApiBaseUrl;
+            var apiTestsOptions = TestConfig.Instance.GetFromSection<AcceptanceTestConfiguration>("AcceptanceTestSettings");
+            testContext.BaseUrl = apiTestsOptions.UserApiBaseUrl;
         }
 
         [BeforeTestRun]
