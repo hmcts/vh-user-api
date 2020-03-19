137545c9fa7df165a24849ce82ed72afb482d1ce
Valentina <Valentina.bennett@hmcts.net>
2020-03-19 15:58:38 +0000

    feature/VIH-5603 : Mutation issues after merging with master

--- UserApi/UserApi.UnitTests/Controllers/HealthCheckControllerTests.cs
+++ UserApi/UserApi.UnitTests/Controllers/HealthCheckControllerTests.cs
@@ -42,30 +42,43 @@ namespace UserApi.UnitTests.Controllers
             _userAccountService
                 .Setup(x => x.GetUserByFilterAsync(filter))
                 .ThrowsAsync(new UserServiceException(message, reason));
+
             var result = await _controller.Health();
             
             var typedResult = (ObjectResult) result;
             typedResult.StatusCode.Should().Be((int) HttpStatusCode.InternalServerError);
             var response = (UserApiHealthResponse) typedResult.Value;
             response.UserAccessHealth.Successful.Should().BeFalse();
-            response.UserAccessHealth.ErrorMessage.Should().Be($"{message}: {reason} - {reason}");
+            response.UserAccessHealth.ErrorMessage.Should().NotBeNullOrEmpty();
+            response.UserAccessHealth.ErrorMessage.Should().Be($"{message}: {reason}");
+
+            response.GroupAccessHealth.Successful.Should().BeTrue();
+            response.GroupAccessHealth.ErrorMessage.Should().BeNullOrWhiteSpace();
+            response.GroupAccessHealth.Data.Should().BeNullOrEmpty();
         }
         
         [Test]
         public async Task Should_return_server_error_when_unable_to_access_groups()
         {
-            var message = "GetGroupByName unauthorized access to Microsoft Graph";
-            var reason = "service not available";
+            var message = "";
+            var reason = "";
             _userAccountService
                 .Setup(x => x.GetGroupByNameAsync(It.IsAny<string>()))
                 .ThrowsAsync(new UserServiceException(message, reason));
+
             var result = await _controller.Health();
             
             var typedResult = (ObjectResult) result;
             typedResult.StatusCode.Should().Be((int) HttpStatusCode.InternalServerError);
             var response = (UserApiHealthResponse) typedResult.Value;
+
+            response.UserAccessHealth.Successful.Should().BeTrue();
+            response.UserAccessHealth.ErrorMessage.Should().BeNullOrEmpty();
+            response.UserAccessHealth.Data.Should().BeNullOrEmpty();
+
             response.GroupAccessHealth.Successful.Should().BeFalse();
-            response.GroupAccessHealth.ErrorMessage.Should().NotBeNullOrWhiteSpace();
+            response.GroupAccessHealth.ErrorMessage.Should().NotBeNullOrEmpty();
+            response.GroupAccessHealth.ErrorMessage.Should().Be($"{message}: {reason}");
         }
 
         [Test]
@@ -86,17 +99,49 @@ namespace UserApi.UnitTests.Controllers
             response.GroupAccessHealth.Data.Should().BeNullOrEmpty();
 
             _userAccountService.Verify(u => u.GetGroupByNameAsync("TestGroup"), Times.Once);
-
+            _userAccountService.Verify(u => u.GetUserByFilterAsync(It.IsAny<string>()), Times.Once);
         }
 
         [Test]
         public async Task should_return_the_application_version_from_assembly()
         {
             var result = await _controller.Health();
+
             var typedResult = (ObjectResult)result;
+            typedResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
             var response = (UserApiHealthResponse)typedResult.Value;
             response.AppVersion.FileVersion.Should().NotBeNullOrEmpty();
             response.AppVersion.InformationVersion.Should().NotBeNullOrEmpty();
         }
+
+        [Test]
+        public async Task Should_return_server_errors_if_access_to_user_and_croup_unsuccessful()
+        {
+            var email = "checkuser@test.com";
+            var filter = $"otherMails/any(c:c eq '{email}')";
+            var message = string.Empty; 
+            var reason = string.Empty;
+            _userAccountService
+               .Setup(x => x.GetGroupByNameAsync(It.IsAny<string>()))
+               .ThrowsAsync(new UserServiceException(message, reason));
+            _userAccountService
+                .Setup(x => x.GetUserByFilterAsync(filter))
+                .ThrowsAsync(new UserServiceException(message, reason));
+
+            var result = await _controller.Health();
+
+            var typedResult = (ObjectResult)result;
+            typedResult.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
+            var response = (UserApiHealthResponse)typedResult.Value;
+            response.UserAccessHealth.Successful.Should().BeFalse();
+            response.UserAccessHealth.ErrorMessage.Should().NotBeNullOrEmpty();
+            response.UserAccessHealth.ErrorMessage.Should().Be($"{message}: {reason}");
+
+            response.GroupAccessHealth.Successful.Should().BeFalse();
+            response.GroupAccessHealth.ErrorMessage.Should().NotBeNullOrWhiteSpace();
+            response.GroupAccessHealth.ErrorMessage.Should().Be($"{message}: {reason}");
+
+        }
+
     }
 }
