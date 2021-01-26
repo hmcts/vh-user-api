22373ae00ba9300143173874de2ba67445bdc208
Chris T <2236507+tremolo64@users.noreply.github.com>
2021-01-26 13:15:39 +0000

    Add key-per-file configuration to vh-user-api (#103)
    Co-authored-by: Chris Tumolo <chris.tumolo@HMCTS.net>

--- UserApi/UserApi/Program.cs
+++ UserApi/UserApi/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace UserApi
@@ -14,11 +17,24 @@ namespace UserApi
         private static IHostBuilder CreateWebHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
                     webBuilder.UseIISIntegration();
                     webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder.ConfigureAppConfiguration((_, configuration) => AddAllKeysPerFileForKubernetes(configuration));
+                })
+                .ConfigureAppConfiguration((_, configuration) => AddAllKeysPerFileForKubernetes(configuration));
+        }
+
+        public static void AddAllKeysPerFileForKubernetes(this IConfigurationBuilder configuration)
+        {
+            const string rootPath = "/mnt/secret/vh-infra-core/vh-user-api";
+
+            if (Directory.Exists(rootPath))
+            {
+                configuration.AddKeyPerFile(directoryPath: rootPath, optional: true);
+            }
         }
     }
 }
