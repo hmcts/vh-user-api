2f93192a73ca553630f60962dc3a661347ae0a2a
Levi <levi.butler@hmcts.net>
2021-02-02 12:07:43 +0000

    added

--- UserApi/UserApi/Program.cs
+++ UserApi/UserApi/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using VH.Core.Configuration;
 
 namespace UserApi
 {
@@ -20,6 +21,7 @@ namespace UserApi
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .AddAksKeyVaultSecretProvider()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
