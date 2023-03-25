922543dffaac3e1aa8f077fdfa950157bc25548e
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-25 14:27:35 +0000

    run dockerfile

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -4,8 +4,6 @@ set -x
 rm -d -r ${PWD}/Coverage
 rm -d -r ${PWD}/TestResults
 
-dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /s:"${PWD}/vh-api-sonar-settings.xml"
-
 exclusions="[Testing.Common]*,[UserApi.*Tests?]*,[UserApi.API]Startup,[UserApi.Common]*"
 configuration=Release
 
@@ -25,5 +23,3 @@ dotnet test UserApi/UserApi.UnitTests/UserApi.UnitTests.csproj -c $configuration
 #     "/p:CoverletOutput=${PWD}/Coverage/" \
 #     "/p:MergeWith=${PWD}/Coverage/coverage.json" \
 #     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
-
-dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
