5ce2b17f621dfa61a1a6d7ca9b3d88f72ddaa851
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-04-11 12:14:41 +0100

    update config for dotnet coverage

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 set -x
 
-rm -d -r ${PWD}/Coverage
-rm -d -r ${PWD}/TestResults
-
 exclusions="[Testing.Common]*,[UserApi.*Tests?]*,[UserApi.API]Startup,[UserApi.Common]*"
 configuration=Release
 
 dotnet build UserApi/UserApi.sln -c $configuration
 # Script is for docker compose tests where the script is at the root level
-dotnet test UserApi/UserApi.UnitTests/UserApi.UnitTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=UserApi-Unit-Tests-TestResults.trx" \
+dotnet test UserApi/UserApi.UnitTests/UserApi.UnitTests.csproj -c $configuration --results-directory ./TestResults --logger "trx;LogFileName=UserApi-Unit-Tests-TestResults.trx" \
     "/p:CollectCoverage=true" \
     "/p:Exclude=\"${exclusions}\"" \
     "/p:CoverletOutput=${PWD}/Coverage/" \
@@ -17,7 +14,7 @@ dotnet test UserApi/UserApi.UnitTests/UserApi.UnitTests.csproj -c $configuration
     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
 
 ## Enable this when integration tests are ready
-# dotnet test UserApi/UserApi.IntegrationTests/UserApi.IntegrationTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=UserApi-Integration-Tests-TestResults.trx" \
+# dotnet test UserApi/UserApi.IntegrationTests/UserApi.IntegrationTests.csproj -c $configuration --results-directory ./TestResults --logger "trx;LogFileName=UserApi-Integration-Tests-TestResults.trx" \
 #     "/p:CollectCoverage=true" \
 #     "/p:Exclude=\"${exclusions}\"" \
 #     "/p:CoverletOutput=${PWD}/Coverage/" \
