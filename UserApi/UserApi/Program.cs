f67acdb75266b02c5122c494dfef3b6f46ce2058
William Craig <mr.craig1991@gmail.com>
2025-05-16 15:05:51 +0100

    updated branch name

--- UserApi/UserApi/Program.cs
+++ UserApi/UserApi/Program.cs
@@ -5,52 +5,47 @@ using Microsoft.Extensions.Configuration.KeyPerFile;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
-namespace UserApi
+namespace UserApi;
+
+public static class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        protected Program()
-        {
-        }
+        CreateHostBuilder(args).Build().Run();
+    }
 
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    // ReSharper disable once MemberCanBePrivate.Global Needed for client generation on build with nswag
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        const string vhInfraCore = "/mnt/secrets/vh-infra-core";
+        const string vhUserApi = "/mnt/secrets/vh-user-api";
+        const string vhAdminWeb = "/mnt/secrets/vh-admin-web";
+        var keyVaults = new[] { vhInfraCore, vhUserApi, vhAdminWeb };
 
-        // ReSharper disable once MemberCanBePrivate.Global Needed for client generation on build with nswag
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            const string vhInfraCore = "/mnt/secrets/vh-infra-core";
-            const string vhUserApi = "/mnt/secrets/vh-user-api";
-            const string vhAdminWeb = "/mnt/secrets/vh-admin-web";
-            var keyVaults = new[] { vhInfraCore, vhUserApi, vhAdminWeb };
-
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((configBuilder) =>
-                {
-                    foreach (var keyVault in keyVaults.Where(Directory.Exists))
-                    {
-                        configBuilder.Add(GetKeyPerFileSource(keyVault));
-                    }
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
-                    webBuilder.UseIISIntegration();
-                    webBuilder.UseStartup<Startup>();
-                });
-        }
-        
-        private static KeyPerFileConfigurationSource GetKeyPerFileSource(string filePath)
-        {
-            return new KeyPerFileConfigurationSource
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((configBuilder) =>
             {
-                FileProvider = new PhysicalFileProvider(filePath),
-                Optional = true,
-                ReloadOnChange = true,
-                SectionDelimiter = "--" // Set your custom delimiter here
-            };
-        }
+                foreach (var keyVault in keyVaults.Where(Directory.Exists))
+                {
+                    configBuilder.Add(GetKeyPerFileSource(keyVault));
+                }
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                webBuilder.UseIISIntegration();
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+        
+    private static KeyPerFileConfigurationSource GetKeyPerFileSource(string filePath)
+    {
+        return new KeyPerFileConfigurationSource
+        {
+            FileProvider = new PhysicalFileProvider(filePath),
+            Optional = true,
+            ReloadOnChange = true,
+            SectionDelimiter = "--" // Set your custom delimiter here
+        };
     }
 }
